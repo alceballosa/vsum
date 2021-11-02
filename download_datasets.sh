a8e13f835f7c1b7ed7db3e236801bc3fa9222b82
druiz35 <67380938+druiz35@users.noreply.github.com>
2021-11-02 12:12:59 -0500

    Update download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -5,4 +5,4 @@ mkdir videosum_datasets/tvsum
 wget -P videosum_datasets/summe/ https://data.vision.ee.ethz.ch/cvl/SumMe/SumMe.zip 
 unzip videosum_datasets/summe/SumMe.zip
 wget -P videosum_datasets/tvsum/ http://people.csail.mit.edu/yalesong/tvsum/tvsum50_ver_1_1.tgz 
-tar -xvzf videosum_datasets/summe/tvsum50_ver_1_1.tgz
+tar -xvzf videosum_datasets/tvsum/tvsum50_ver_1_1.tgz
